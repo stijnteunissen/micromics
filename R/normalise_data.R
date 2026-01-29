28800f36d90cf94923822c9a2933ebe4b890327e
stijnteunissen <stijnteunissen@ziggo.nl>
2026-01-29 12:45:42 +0100

    update

--- R/normalise_data.R
+++ R/normalise_data.R
@@ -475,7 +475,7 @@ normalise_data = function(physeq = without_mock_physeq,
     return(list(psdata_asv_copy_number_corrected = psdata_copy_number_corrected, psdata_asv_qpcr_norm = psdata_qpcr_norm))
 
   } else {
-    log_message("Error: Invalid normalization method specified. Use 'fcm' or 'qpcr'.", log_file)
+    log_message("Error: Invalid normalization method specified. Use 'fcm' or 'qpcr' or NULL.", log_file)
   }
 }
 
